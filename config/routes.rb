e3cb6dc26ba1d4617874f348cd455cfea8ef7716
PuZZleDucK <puzzleduck@gmail.com>
2020-07-29 15:01:39 +1000

    add subdomain routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  constraints(Subdomain) do
+    root to: 'companies#index'
+    resources :companies
+  end
+  root to: 'companies#index'
+
+  # root to: 'companies#index'
+  # resources :companies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
